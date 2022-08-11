f2d2d0c064306440bec14d99635c0a9370d371c9
= <=>
2022-08-11 12:23:43 -0500

    d

--- assets/js/app.js
+++ assets/js/app.js
@@ -94,7 +94,7 @@ popupWhatsApp = () => {
       let msg = document.getElementById('whats-in').value;
       let relmsg = msg.replace(/ /g, "%20");
       //just change the numbers "1515551234567" for your number. Don't use +001-(555)1234567     
-      window.open('https://wa.me/573202844903?text=' + relmsg, '_blank');
+      window.open('https://wa.me/573203528598?text=' + relmsg, '_blank');
 
    });
 
